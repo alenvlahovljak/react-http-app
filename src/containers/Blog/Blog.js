b3e3337b7874ed490427a8d6ebcd958816412f09
Alen <alenvlahovljak7@gmail.com>
2020-03-11 08:19:36 +0100

    Inital commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+//import axios from "axios";
+import axios from "../../axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import post from "../../components/Post/Post";
+
+class Blog extends Component {
+	state = {
+		posts: [],
+		selectedPostId: null,
+		err: false
+	};
+	componentDidMount() {
+		axios
+			.get("/posts")
+			.then(res => {
+				const posts = res.data.slice(1, 4);
+				const updatedPosts = posts.map(post => {
+					return {
+						...post,
+						auth: "Alen"
+					};
+				});
+				this.setState({ posts: updatedPosts, err: false });
+			})
+			.catch(err => {
+				this.setState({ err: true });
+			});
+	}
+	postSelectedHandler = id => {
+		this.setState({ selectedPostId: id });
+	};
+	render() {
+		let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
+		if (!this.state.err) {
+			posts = this.state.posts.map(post => {
+				return (
+					<Post
+						key={post.id}
+						title={post.title}
+						author={post.author}
+						clicked={this.postSelectedHandler.bind(this, post.id)}
+					/>
+				);
+			});
+		}
+		return (
+			<div>
+				<section className="Posts">{posts}</section>
+				<section>
+					<FullPost id={this.state.selectedPostId} />
+				</section>
+				<section>
+					<NewPost />
+				</section>
+			</div>
+		);
+	}
+}
+
+export default Blog;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+	reqConfig => {
+		console.log(reqConfig);
+		//return the request config, otherwise we are blocking the request
+		return reqConfig;
+	},
+	err => {
+		console.log(err);
+		return Promise.reject(err);
+	}
+);
+
+axios.interceptors.response.use(
+	res => {
+		console.log(res);
+		return res;
+	},
+	err => {
+		console.log(err);
+		return Promise.reject(err);
+	}
+);
+
+//Remove Interceptors
+//var myInterceptor = axios.interceptors.request.use(function () {/*...*/});
+//axios.interceptors.request.eject(myInterceptor);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
